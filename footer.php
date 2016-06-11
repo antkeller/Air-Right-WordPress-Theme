96e8659a5b670b1d8b9a400bc08d5839b5da4387
Anthony Keller <antkeller1@gmail.com>
2016-06-10 19:40:54 -0700

    Add google analytics to footer

--- footer.php
+++ footer.php
@@ -1,15 +1,13 @@
-<?php
-/**
- * The template for displaying the footer
- *
- * @package WordPress
- * @subpackage MervinMfg-WordPress-Theme-Template
- * @since Mervin Mfg WordPress Theme Template 1.0.0
- */
-?>
-		<footer id="footer" class="source-org vcard copyright" role="contentinfo">
-			<small>&copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?></small>
-		</footer>
+			</div><!-- END #main -->
+			<footer id="footer" class="source-org vcard copyright" role="contentinfo">
+				<div class="footer-inside">
+					<nav id="nav" class="primary-navigation" role="navigation">
+						<?php wp_nav_menu( array('theme_location' => 'footer') ); ?>
+					</nav>
+					<small class="copyright">All content &copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?> <span>Products and Services LLC</span></small>
+				</div><!-- .footer-inside -->
+			</footer>
+		</div><!-- END .wrapper-inside -->
 	</div><!-- END #wrapper -->
 
 	<div class="responsive-check">
@@ -21,58 +19,23 @@
 
 	<!-- JavaScript includes -->
 <?php include '_/inc/footer-includes.php' ?>
-	
+
 	<!-- Init the main JS -->
 	<script type="text/javascript">
 		$(document).ready(function(){
-			APP.Main.init();
+			AIRRIGHT.Main.init();
 		});
 	</script>
-	<!-- Social Media Includes -->
-	<!--
-	<div id="fb-root"></div>
-	<script type="text/javascript">
-		// Facebook
-		(function(d, s, id) {
-			var js, fjs = d.getElementsByTagName(s)[0];
-			if (d.getElementById(id)) return;
-			js = d.createElement(s); js.id = id;
-			js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=XXXXXXXXXX";
-			fjs.parentNode.insertBefore(js, fjs);
-		}(document, 'script', 'facebook-jssdk'));
-		// Twitter
-		!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
-		// Google+
-		(function() {
-			var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-			po.src = 'https://apis.google.com/js/plusone.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-		})();
-		// Pinterest
-		(function(d){
-			var f = d.getElementsByTagName('SCRIPT')[0], p = d.createElement('SCRIPT');
-			p.type = 'text/javascript';
-			p.async = true;
-			p.src = '//assets.pinterest.com/js/pinit.js';
-			f.parentNode.insertBefore(p, f);
-		}(document));
-	</script>
-	-->
 	<!-- Google Analytics -->
-	<!--
-	<script type="text/javascript">
-		var _gaq = _gaq || [];
-		_gaq.push(['_setAccount', 'UA-XXXXXXXX-X']);
-		_gaq.push(['_setDomainName', '.XXXXXXXXX.com']);
-		_gaq.push(['_setAllowHash', false]);
-		_gaq.push(['_setAllowLinker', true]);
-		_gaq.push(['_trackPageview']);
-		(function() {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		})();
-	</script>
-	-->
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-79155257-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </body>
 </html>
